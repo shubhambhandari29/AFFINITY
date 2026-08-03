68f7407546ce6c5de2e15f78a692acaa5abfb9a0
Shubham Bhandari <83971506+shubhambhandari29@users.noreply.github.com>
2026-08-03 15:40:14 +0530

    Update hcm_account_associations.py

--- core/models/hcm_account_associations.py
+++ core/models/hcm_account_associations.py
@@ -3,8 +3,6 @@ from pydantic import BaseModel
 
 class HCMAccountAssociationUpsert(BaseModel):
     CustomerNum: str | None = None
-    AccountNum: str | None = None
-    AssociationType: str | None = None
 
     class Config:
         extra = "allow"
